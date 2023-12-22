c46b7a5918e9a1020858b248be698f1d2ad76b73
Rafal <136376818+nikczemnydev@users.noreply.github.com>
2023-12-22 13:36:44 +0100

    Made print statements more readable and looking exactly the same as in the recursive solution. Added comments. Changed the initial hardcoded value of the position until which the sequence is printed to 31, same as my recursive variant (it had a number limit instead, it printed numbers in the sequence up to a million value, which is 31 positions.)

--- src/FibonacciNonRecursive.java
+++ src/FibonacciNonRecursive.java
@@ -1,15 +1,21 @@
 public class FibonacciNonRecursive {
     public static void main(String[] args) {
-        int nthPos = 20;
+        // calculating up to a 31st position - not an accident, my recursive
+        // solution used a number instead - it printed the sequence as long
+        // as the numbers stayed under 1 million, that is 31 positions.  : )
+        int nthPos = 31;
+        // initializing a and b with first 2 numbers of the sequence: 0 and 1
         long a = 0;
         long b = 1;
 
         System.out.println("Fibonacci Sequence up to " + nthPos + " position:");
-        System.out.print(a + " " + b);
-
+        System.out.print(a + ", " + b);
+        // loop iterates from 2 to the value of nthPos, calculating and
+        // printing each number. a b and c are updated iteratively avoiding
+        // recursion
         for (int i = 2; i < nthPos; i++) {
             long c = a + b;
-            System.out.print(" " + c);
+            System.out.print(", " + c);
             a = b;
             b = c;
         }
